e736b2fd13075d1cf2f1d19659e3764ace1085cd
bahadirsofuoglu <bahadirsofuoglu@gmail.com>
2021-06-26 13:21:14 +0300

    card moved to components

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,14 @@
 <template>
-  <div>
-    <ul class="flashcard-list">
-      <li v-for="card in cards" :key="card.front" @click="toggleCard(card)">
-        <transition name="flip">
-          <div class="flipCard">
-            <p class="text-card">
-              {{ card.flipped ? card.back : card.front }}
-            </p>
-          </div>
-        </transition>
-      </li>
-    </ul>
+  <div v-for="card in cards" :key="card.front">
+    <Card :card="card" @click="toggleCard(card)" />
   </div>
 </template>
 
 <script>
 import { ref } from 'vue'
+import Card from '@/components/Card.vue'
 export default {
+  components: { Card },
   setup () {
     const cards = ref([
       {
@@ -59,96 +51,3 @@ export default {
   }
 }
 </script>
-
-<style>
-.body {
-  height: 100%;
-}
-ul {
-  padding-left: 0;
-  display: flex;
-  flex-flow: row wrap;
-}
-li {
-  list-style-type: none;
-  padding: 10px 10px;
-  transition: all 0.3s ease;
-}
-.flipCard {
-  display: block;
-  width: 200px;
-  height: 205px;
-  padding: 80px 50px;
-  background-color: #a17de9;
-  border-radius: 7px;
-  margin: 5px;
-  text-align: center;
-  line-height: 27px;
-  cursor: pointer;
-  position: relative;
-  -webkit-box-shadow: 9px 10px 22px -8px rgba(209, 193, 209, 0.5);
-  -moz-box-shadow: 9px 10px 22px -8px rgba(209, 193, 209, 0.5);
-  box-shadow: 9px 10px 22px -8px rgba(209, 193, 209, 0.5);
-  will-change: transform;
-}
-.text-card {
-  display: block;
-  color: #fff;
-  text-align: inherit;
-  font-weight: 600;
-  font-size: 15px;
-}
-li:hover {
-  transform: scale(1.1);
-}
-li:nth-child(-n + 3) .flipCard {
-  background-color: #e65f51;
-}
-li:nth-child(2n + 1) .flipCard {
-  background-color: #a17de9;
-}
-li:nth-child(4n) .flipCard {
-  background-color: #feca34;
-}
-li:nth-child(5n-2) .flipCard {
-  background-color: #51aae5;
-}
-li:nth-child(4n + 4) .flipCard {
-  background-color: #feca34;
-}
-li:nth-child(-7n + 7) .flipCard {
-  background-color: #e46055;
-}
-.delete-card {
-  position: absolute;
-  right: 0;
-  top: 0;
-  padding: 10px 15px;
-  opacity: 0.4;
-  transition: all 0.5s ease;
-}
-.delete-card:hover,
-.error {
-  opacity: 1;
-  transform: rotate(360deg);
-}
-.flip-enter-active {
-  transition: all 0.4s ease;
-}
-.flip-leave-active {
-  display: none;
-}
-.flip-enter,
-.flip-leave {
-  transform: rotateY(180deg);
-  opacity: 0;
-}
-.form-card {
-  position: absolute;
-  left: 0;
-  top: 0;
-  padding: 10px 15px;
-  opacity: 0.8;
-  transition: all 0.5s ease;
-}
-</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,116 @@
+<template>
+  <div class="flashcard-list" @click="callback">
+    <transition name="flip">
+      <div class="flipCard">
+        <p class="text-card">
+          {{ card.flipped ? card.back : card.front }}
+        </p>
+      </div>
+    </transition>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    card: {
+      type: Object
+    }
+  },
+  emits: ['click'],
+  setup (props, { emit }) {
+    const callback = e => {
+      emit('click', e)
+    }
+    return { callback }
+  }
+}
+</script>
+<style>
+ul {
+  padding-left: 0;
+  display: flex;
+  flex-flow: row wrap;
+}
+li {
+  list-style-type: none;
+  padding: 10px 10px;
+  transition: all 0.3s ease;
+}
+.flipCard {
+  display: block;
+  width: 200px;
+  height: 205px;
+  padding: 80px 50px;
+  background-color: #a17de9;
+  border-radius: 7px;
+  margin: 5px;
+  text-align: center;
+  line-height: 27px;
+  cursor: pointer;
+  position: relative;
+  -webkit-box-shadow: 9px 10px 22px -8px rgba(209, 193, 209, 0.5);
+  -moz-box-shadow: 9px 10px 22px -8px rgba(209, 193, 209, 0.5);
+  box-shadow: 9px 10px 22px -8px rgba(209, 193, 209, 0.5);
+  will-change: transform;
+}
+.text-card {
+  display: block;
+  color: #fff;
+  text-align: inherit;
+  font-weight: 600;
+  font-size: 15px;
+}
+li:hover {
+  transform: scale(1.1);
+}
+li:nth-child(-n + 3) .flipCard {
+  background-color: #e65f51;
+}
+li:nth-child(2n + 1) .flipCard {
+  background-color: #a17de9;
+}
+li:nth-child(4n) .flipCard {
+  background-color: #feca34;
+}
+li:nth-child(5n-2) .flipCard {
+  background-color: #51aae5;
+}
+li:nth-child(4n + 4) .flipCard {
+  background-color: #feca34;
+}
+li:nth-child(-7n + 7) .flipCard {
+  background-color: #e46055;
+}
+.delete-card {
+  position: absolute;
+  right: 0;
+  top: 0;
+  padding: 10px 15px;
+  opacity: 0.4;
+  transition: all 0.5s ease;
+}
+.delete-card:hover,
+.error {
+  opacity: 1;
+  transform: rotate(360deg);
+}
+.flip-enter-active {
+  transition: all 0.4s ease;
+}
+.flip-leave-active {
+  display: none;
+}
+.flip-enter,
+.flip-leave {
+  transform: rotateY(180deg);
+  opacity: 0;
+}
+.form-card {
+  position: absolute;
+  left: 0;
+  top: 0;
+  padding: 10px 15px;
+  opacity: 0.8;
+  transition: all 0.5s ease;
+}
+</style>
