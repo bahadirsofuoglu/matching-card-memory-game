0795be5353ef2de91cdbf2c374d8e9a865e46526
bahadirsofuoglu <bahadirsofuoglu@gmail.com>
2021-06-28 16:57:31 +0300

    firebaseConfig bug fixed

--- src/data/firebaseConfig.js
+++ src/data/firebaseConfig.js
@@ -1,21 +1,9 @@
-/* export const firebaseConfig = {
-  apiKey: process.env.VUE_APP_APIKEY,
-  authDomain: process.env.VUE_APP_AUTHDOMAIN,
-  databaseURL: process.env.VUE_APP_DATABASEURL,
-  projectId: process.env.VUE_APP_PROJECTID,
-  storageBucket: process.env.VUE_APP_STORAGEBUCKET,
-  messagingSenderId: process.env.VUE_APP_MESSAGINGSENDERID,
-  appId: process.env.VUE_APP_APPID
-} */
-
 import firebase from 'firebase'
-
-/* const settings = { timestampsInSnapshots: true } */
+const settings = { timestampsInSnapshots: true }
 
 const config = {
   apiKey: process.env.VUE_APP_APIKEY,
   authDomain: process.env.VUE_APP_AUTHDOMAIN,
-  databaseURL: process.env.VUE_APP_DATABASEURL,
   projectId: process.env.VUE_APP_PROJECTID,
   storageBucket: process.env.VUE_APP_STORAGEBUCKET,
   messagingSenderId: process.env.VUE_APP_MESSAGINGSENDERID,
@@ -23,6 +11,6 @@ const config = {
 }
 firebase.initializeApp(config)
 
-/* firebase.firestore().settings(settings) */
+firebase.firestore().settings(settings)
 
 export default firebase
