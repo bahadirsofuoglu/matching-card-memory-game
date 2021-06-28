2332fa5d2c337e594ef878ace908f0bd2d7ad7d5
bahadirsofuoglu <bahadirsofuoglu@gmail.com>
2021-06-29 02:14:39 +0300

    transition deleted

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,17 +1,12 @@
 <template>
   <div class="flashcard-list" @click="callback">
-    <transition name="flip">
-      <div
-        class="flipCard"
-        :style="card.flipped ? 'background-color:black' : ''"
-      >
-        <font-awesome-icon
-          v-if="card.flipped"
-          v-bind:key="card.flipped"
-          :icon="card.icon"
-        />
-      </div>
-    </transition>
+    <div class="flipCard" :style="card.flipped ? 'background-color:black' : ''">
+      <font-awesome-icon
+        v-if="card.flipped"
+        v-bind:key="card.flipped"
+        :icon="card.icon"
+      />
+    </div>
   </div>
 </template>
 <script>
@@ -44,8 +39,7 @@ export default {
 
   min-width: 60px;
   min-height: 60px;
-  /* max-width: 200px;
-  max-height: 200px; */
+
   padding: 40px 20px;
   border-radius: 7px;
   margin: 10px;
@@ -68,26 +62,5 @@ export default {
 }
 .flashcard-list:hover {
   transform: scale(1.1);
-}
-
-flip,
-.flip-enter-active {
-  transition: all 0.4s ease;
-}
-.flip-leave-active {
-  display: none;
-}
-.flip-enter,
-.flip-leave {
-  transform: rotateY(180deg);
-  opacity: 0;
-}
-.form-card {
-  position: absolute;
-  left: 0;
-  top: 0;
-  padding: 10px 15px;
-  opacity: 0.8;
-  transition: all 0.5s ease;
 }
 </style>
