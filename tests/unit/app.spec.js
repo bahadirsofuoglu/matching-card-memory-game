948732ba2691149ae4e3b777a360f2e01b1ba8bf
bahadirsofuoglu <bahadirsofuoglu@gmail.com>
2021-06-29 18:35:44 +0300

    toogleCard function test completed

---
+++ tests/unit/app.spec.js
@@ -0,0 +1,31 @@
+import { shallowMount } from '@vue/test-utils'
+import App from '@/App.vue'
+
+const wrapper = shallowMount(App, {})
+const firstCard = { icon: 'coffee', flipped: false }
+const secondCard = { icon: 'tea', flipped: false }
+
+describe('Card Component Tests', () => {
+  describe('Functions Test', () => {
+    it('toogleCard should change some values', async () => {
+      await wrapper.vm.toggleCard(firstCard)
+
+      expect(wrapper.vm.clickCount).toBe(1)
+      expect(firstCard.flipped).toBe(true)
+      /*  expect(wrapper.vm.firstSelectedCard).toEqual({
+        icon: 'coffee',
+        flipped: false
+      }) */
+      expect(wrapper.vm.firstCardSelected).toBe(true)
+    })
+
+    /*    it('matchCards should change some values', async () => {
+      await wrapper.vm.toggleCard(secondCard)
+
+      expect(wrapper.vm.clickCount).toBe(2)
+      expect(secondCard.flipped).toBe(true)
+      
+      expect(wrapper.vm.firstCardSelected).toBe(true)
+    }) */
+  })
+})
