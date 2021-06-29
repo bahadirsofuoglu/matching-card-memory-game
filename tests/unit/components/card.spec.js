2a9b917a1e2f9ba8254bf925925e4c19d7eba642
bahadirsofuoglu <bahadirsofuoglu@gmail.com>
2021-06-29 15:00:53 +0300

    computed properties completed - fontawesomeIcon problem fixed

---
+++ tests/unit/components/card.spec.js
@@ -0,0 +1,29 @@
+import { shallowMount } from '@vue/test-utils'
+import Card from '@/components/Card.vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(fas)
+
+const wrapper = shallowMount(Card, {
+  global: {
+    stubs: { FontAwesomeIcon }
+  },
+  propsData: {
+    card: {}
+  }
+})
+
+describe('Card Component Tests', () => {
+  describe('Computed test', () => {
+    it('computed value flipCardClass should change with card.flipped(true)', async () => {
+      await wrapper.setProps({ card: { icon: 'coffee', flipped: true } })
+      expect(wrapper.vm.flipCardClass).toBe('flipCard bg-black')
+    })
+    it('computed value flipCardClass should change with card.flipped(false)', async () => {
+      await wrapper.setProps({ card: { icon: 'coffee', flipped: false } })
+      expect(wrapper.vm.flipCardClass).toBe('flipCard')
+    })
+  })
+})
