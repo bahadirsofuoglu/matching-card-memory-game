ed91431168ac19b7f0a8f7697bd1c61d87d3c82b
bahadirsofuoglu <bahadirsofuoglu@gmail.com>
2021-06-29 23:38:53 +0300

    matchCard func test

--- tests/unit/app.spec.js
+++ tests/unit/app.spec.js
@@ -2,10 +2,14 @@ import { shallowMount } from '@vue/test-utils'
 import App from '@/App.vue'
 
 const wrapper = shallowMount(App, {})
-const firstCard = { icon: 'coffee', flipped: false }
-const secondCard = { icon: 'tea', flipped: false }
 
 describe('Card Component Tests', () => {
+  let firstCard
+  let secondCard
+  beforeEach(() => {
+    firstCard = { icon: 'coffee', flipped: false }
+    secondCard = { icon: 'tea', flipped: false }
+  })
   describe('Functions Test', () => {
     it('toogleCard should change some values', async () => {
       await wrapper.vm.toggleCard(firstCard)
@@ -18,21 +22,33 @@ describe('Card Component Tests', () => {
       }) */
       expect(wrapper.vm.firstCardSelected).toBe(true)
     })
+    describe('Functions Test', () => {
+      it('Some values should be changed with matchCards function (if state firs-second card icon not match)', async () => {
+        await wrapper.vm.matchCards(secondCard)
 
-    it('matchCards should change some values', async () => {
-      /*   await wrapper.vm.toggleCard(firstCard) */
-      await wrapper.vm.matchCards(secondCard)
+        expect(wrapper.vm.clickCount).toBe(2)
+        expect(secondCard.flipped).toBe(true)
+        window.setTimeout(() => {
+          expect(secondCard.flipped).toBe(false)
+          expect(wrapper.vm.firstSelectedCard).toBe(false)
+          expect(wrapper.vm.lives).toBe(14)
+        }, 1000)
+        expect(wrapper.vm.firstCardSelected).toBe(false)
+        window.setTimeout(() => {
+          expect(wrapper.vm.clickCount).toBe(0)
+        }, 1000)
+        window.setTimeout(() => {
+          expect(wrapper.vm.calculateScore).toHaveBeenCalled()
+        }, 1000)
+      })
 
-      expect(wrapper.vm.clickCount).toBe(2)
-      expect(secondCard.flipped).toBe(true)
+      it('lives should not be changed if first and second cards icons not matched', async () => {
+        secondCard.icon = 'coffee'
+        wrapper.vm.firstSelectedCard = firstCard
+        await wrapper.vm.matchCards(secondCard)
 
-      expect(wrapper.vm.firstCardSelected).toBe(false)
-      window.setTimeout(() => {
-        expect(wrapper.vm.clickCount).toBe(0)
-      }, 1000)
-      window.setTimeout(() => {
-        expect(wrapper.vm.calculateScore).toHaveBeenCalled()
-      }, 1000)
+        expect(wrapper.vm.lives).toEqual(15)
+      })
     })
   })
 })
